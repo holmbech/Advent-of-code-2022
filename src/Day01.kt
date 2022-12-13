2f940bb473c1522e558d964e03bee2d4dfcbf6b9
Anders Holmbech Brandt <anders@holmbech.com>
2022-12-13 18:20:25 +0100

    added day2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,26 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var max = 0
+        var temp = 0
+        for (s in input) {
+            if(s.isEmpty()) {
+                if (temp > max) {
+                    max = temp
+                }
+                temp = 0
+            } else {
+                temp += s.toInt()
+            }
+        }
+        return max
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
+    val input = readInput("Day01_test")
     println(part1(input))
     println(part2(input))
+
 }
